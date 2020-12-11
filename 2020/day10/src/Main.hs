470b9c7814bcd276e003c5b9aa4d041518cbe3ae
Michael Gold <mgold@spscommerce.com>
2020-12-10 20:33:46 -0500

    Working code for part2, too slow

--- 2020/day10/src/Main.hs
+++ 2020/day10/src/Main.hs
@@ -6,31 +6,24 @@ import Control.Arrow (Arrow ((&&&)))
 import Control.Monad ((>=>))
 import Data.List (delete, sort)
 
+paths :: [Int] -> [[(Int, Int)]]
+paths adapters = go maxAdapter 0 adapters
+  where
+    maxAdapter = maximum adapters + 3
+    go _ _ [] = [[]]
+    go max current _ | current + 3 == max = [[]]
+    go max current joltages = do
+      possibility <- filter (\j -> j - current < 4 && j - current >= 0) joltages
+
+      ((possibility, possibility - current) :) <$> go max possibility (delete possibility joltages)
+
 safeMinimum :: Ord a => [a] -> Maybe a
 safeMinimum a
   | null a = Nothing
   | otherwise = Just $ minimum a
 
-getPath :: (Num b, Ord b) => [b] -> [(b, b)]
-getPath joltages =
-  (\lst -> (0, minimum (snd <$> lst)) : lst) $
-    head $
-      filter (\track -> length track == length joltages) $ getPaths joltages
-
-getPaths :: (Num b, Ord b) => [b] -> [[(b, b)]]
-getPaths joltages = go joltages adapter [[]]
-  where
-    adapter = 3 + maximum joltages
-    go _ 0 accum = accum
-    go [] _ accum = accum
-    go js target (latest : rest) =
-      let possibilities = filter (\j -> target - j < 4 && target - j >= 0) js
-       in do
-            possibility <- possibilities -- trace ("target: " <> show target <> " possibilities: " <> show possibilities) possibilities
-            go (delete possibility js) (target - (target - possibility)) (((possibility, target - possibility) : latest) : rest)
-
 part1 :: [Int] -> Int
-part1 =
+part1 input =
   uncurry (*)
     . foldr
       ( \(_, diff) (one, three) ->
@@ -39,17 +32,24 @@ part1 =
             2 -> (one, three)
             3 -> (one, three + 1)
       )
-      (0, 0)
-    . getPath
-    . reverse
+      (0, 1)
+    . head
+    . filter (\l -> length l == length input)
+    . paths
     . sort
+    $ input
 
 part2 :: [Int] -> Int
-part2 = const 0
+part2 =
+  length . paths
+    . sort
 
 solve :: FilePath -> IO ()
 solve =
   readFile >=> print . (part1 &&& part2) . (read <$>) . lines
+
+writeSorted :: FilePath -> FilePath -> IO ()
+writeSorted dest = readFile >=> writeFile dest . unlines . (show <$>) . reverse . sort . ((read <$>) :: [String] -> [Int]) . lines
 
 main :: IO ()
 main = solve "input.txt"
