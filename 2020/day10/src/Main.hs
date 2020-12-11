28b2a4e3e0e7174844a6e3d6fd95bfd1ea57bbed
Michael Gold <mgold@spscommerce.com>
2020-12-10 23:56:49 -0500

    Changing up to use monad syntax instead of comprehensions

--- 2020/day10/src/Main.hs
+++ 2020/day10/src/Main.hs
@@ -5,7 +5,7 @@
 module Main where
 
 import Control.Arrow (Arrow ((&&&)))
-import Control.Monad ((>=>))
+import Control.Monad (guard, (>=>))
 import Data.List (delete, sort)
 
 paths :: [Int] -> [[(Int, Int)]]
@@ -46,11 +46,27 @@ part1 input =
     . sort
     $ input
 
--- Not mine, I intend to rewrite using different mechanisms, but this is from SimonBaars
+-- Initially from SimonBaars, I couldn't figure out how to get my thoughts to code performantly
+-- but I changed to use list monad instead of comprehensions
 part2 :: [Int] -> Integer
-part2 input = head $ foldr (\i acc -> sum [acc !! (j - i -1) | j <- [i + 1 .. i + 3], j < length n, (n !! j) - (n !! i) <= 3] : acc) [1] [0 .. length n -2]
-  where
-    n = sort $ maximum input + 3 : 0 : input
+part2 input =
+  let totalInputs = sort $ maximum input + 3 : 0 : input
+   in head $
+        foldr
+          ( \i acc ->
+              -- Get the total number of paths possible from here, and sum them together,
+              -- appending to our running list
+              sum
+                ( do
+                    j <- [i + 1 .. i + 3]
+                    guard $ j < length totalInputs -- Valid values
+                    guard $ (totalInputs !! j) - (totalInputs !! i) <= 3
+                    pure $ acc !! (j - i - 1)
+                ) :
+              acc
+          )
+          [1] -- This seeds the list
+          [0 .. length totalInputs - 2]
 
 solve :: FilePath -> IO ()
 solve =
