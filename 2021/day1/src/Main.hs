55b83640a2c737bbef90226e9ab2eb93e011f902
Michael Gold <mgold@spscommerce.com>
2021-12-02 08:59:23 -0500

    Better solution

--- 2021/day1/src/Main.hs
+++ 2021/day1/src/Main.hs
@@ -6,23 +6,15 @@ import Debug.Trace
 type Input = [Int]
 
 part1 :: Input -> Int
-part1 =
-  snd
-    . foldl
-      ( \(prev, num) i -> case prev of
-          Just p | i > p -> (Just i, num + 1)
-          _ -> (Just i, num)
-      )
-      (Nothing, 0)
+part1 (x : y : xs) = if y > x then 1 + part1 (y : xs) else part1 (y : xs)
+part1 _ = 0
 
 part2 :: Input -> Int
 part2 = part1 . chunked []
 
 chunked :: Input -> Input -> Input
-chunked accum [] = accum
-chunked accum [x] = accum
-chunked accum [x, y] = accum
 chunked accum (x : y : z : xs) = chunked (accum ++ [x + y + z]) (y : z : xs)
+chunked accum _ = accum
 
 main :: IO ()
 main = do
