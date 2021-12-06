2efd5cb9ed507c96949f6a104d87ae0968195cba
Michael Gold <mgold@spscommerce.com>
2021-12-06 15:41:39 -0500

    Day 6, Part 2

--- 2021/day6/src/Main.hs
+++ 2021/day6/src/Main.hs
@@ -1,27 +1,31 @@
 module Main where
 
 import Control.Arrow (Arrow ((&&&)))
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Data.List (group, sort)
 import Data.List.Split (splitOn)
 import Data.Maybe (fromMaybe)
 import Safe (headMay)
 import System.Environment (getArgs)
 
-type Input = [Int]
+type Input = IntMap Int
 
---part1 :: Input -> Int
-part1 input = length $ foldr (\_ i -> singleDay i) input [0 .. 79]
+part1 :: Input -> Int
+part1 input = sum $ IntMap.elems $ foldr (const singleDay) input [0 .. 79]
 
 part2 :: Input -> Int
-part2 = const 0
+part2 input = sum $ IntMap.elems $ foldr (const singleDay) input [0 .. 255]
 
-singleDay :: Input -> [Int]
-singleDay = uncurry (++) . foldr foldFn ([], [])
+singleDay :: Input -> Input
+singleDay = IntMap.foldrWithKey foldFn IntMap.empty
  where
-  foldFn 0 (existing, new) = (6 : existing, 8 : new)
-  foldFn n (existing, new) = (n - 1 : existing, new)
+  foldFn 0 existingNum newMap =
+    IntMap.insertWith (+) 8 existingNum (IntMap.insertWith (+) 6 existingNum newMap)
+  foldFn key existing newMap = IntMap.insertWith (+) (key -1) existing newMap
 
 transform :: String -> Input
-transform = (read <$>) . splitOn ","
+transform = IntMap.fromAscList . ((\g -> (head g, length g)) <$>) . group . sort . (read <$>) . splitOn ","
 
 main :: IO ()
 main = do
