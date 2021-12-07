2535f1b53ebf43d63362835438c3c5a74d3a4535
Michael Gold <mgold@spscommerce.com>
2021-12-07 12:20:46 -0500

    Day 7, Part 2

--- 2021/day7/src/Main.hs
+++ 2021/day7/src/Main.hs
@@ -8,13 +8,13 @@ import System.Environment (getArgs)
 
 type Input = [Int]
 
-part1 :: Input -> Maybe (Int, Int)
-part1 input =
+solve :: (Input -> Int -> (Int, Int)) -> Input -> Maybe (Int, Int)
+solve calcFn input =
   let min = minimum input
       max = maximum input
    in foldr
         ( \i mins ->
-            let item@(num, fuel) = calc input i
+            let item@(num, fuel) = calcFn input i
              in case mins of
                   Nothing -> Just item
                   Just (_, fuel') | fuel < fuel' -> Just item
@@ -23,11 +23,20 @@ part1 input =
         Nothing
         [min .. max]
 
+part1 :: Input -> Int
+part1 = maybe 0 snd . solve calc
+
 calc :: Input -> Int -> (Int, Int)
 calc input n = (n, foldr (\i fuel -> abs (i - n) + fuel) 0 input)
 
+calcWithStep :: Input -> Int -> (Int, Int)
+calcWithStep input n = (n, foldr (\i fuel -> fuelCost (abs (i - n)) + fuel) 0 input)
+ where
+  fuelCost diff =
+    sum [0 .. diff]
+
 part2 :: Input -> Int
-part2 = const 0
+part2 = maybe 0 snd . solve calcWithStep
 
 transform :: String -> Input
 transform = (read <$>) . splitOn ","
