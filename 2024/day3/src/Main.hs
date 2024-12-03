4253fcf2f1b22b4de8685929a0236048bb53a091
Michael Gold <mgold@spscommerce.com>
2024-12-03 08:51:55 -0500

    Adding back in cabal file

--- 2024/day3/src/Main.hs
+++ 2024/day3/src/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import Control.Arrow ((&&&))
+
+type Input = ()
+
+part1 :: Input -> Int
+part1 input = 0
+
+part2 :: Input -> Int
+part2 input = 0
+
+makeInput :: String -> Input
+makeInput contents = ()
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  input <- makeInput <$> readFile "sample.txt"
+  print $ (part1 &&& part2) input
