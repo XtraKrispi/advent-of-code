71d88f645f6a0ceb9d233153ed283c7a40ca9349
Michael Gold <mgold@spscommerce.com>
2024-12-04 09:21:59 -0500

    Day 4 initial commit

---
+++ 2024/day4/src/Main.hs
@@ -0,0 +1,19 @@
+module Main where
+
+import Control.Arrow ((&&&))
+
+type Input = ()
+
+part1 :: Input -> Int
+part1 _ = 0
+
+part2 :: Input -> Int
+part2 _ = 0
+
+makeInput :: String -> Input
+makeInput _ = ()
+
+main :: IO ()
+main = do
+  input <- makeInput <$> readFile "sample.txt"
+  print $ (part1 &&& part2) input
