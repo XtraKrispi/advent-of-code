130ecd438f48cafe20976de74e7d4d9e9f28f839
Michael Gold <mgold@spscommerce.com>
2024-12-04 14:06:32 -0500

    Day 4 part 2, still brute force, but it works

--- 2024/day4/src/Main.hs
+++ 2024/day4/src/Main.hs
@@ -2,11 +2,9 @@ module Main where
 
 import Control.Arrow ((&&&))
 import Control.Monad (join)
-import Data.List ((\\))
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe (catMaybes)
-import Debug.Trace (trace)
 
 type Row = Int
 type Col = Int
@@ -21,33 +19,27 @@ data Bounds = Bounds
   }
   deriving (Show)
 
--- part1 :: Input -> Int
--- part1 :: Input -> Int
+part1 :: Input -> Int
 part1 (Input bounds m) =
-  length $ filter (\s -> s == "XMAS" || s == reverse "XMAS") $ join $ (getWords . potentialWords bounds) <$> Map.keys m
+  length $
+    filter (\s -> s == "XMAS" || s == reverse "XMAS") $
+      join $
+        (getWords . potentialWords bounds) <$> Map.keys m
  where
   getWords :: [[Coord]] -> [String]
   getWords ws = (\cs -> catMaybes $ flip Map.lookup m <$> cs) <$> ws
 
---   go 0 (Map.keys m)
---  where
---   go :: Int -> [Coord] -> Int
---   go count [] = count
---   go count (coord : xs) =
---     if isMatch coord then
---       go (count + 1)
---     -- if c == 'X' || c == 'S'
---     --   then case matches coord m of
---     --     [] -> go count xs
---     --     ms -> go (count + 1) (xs \\ join ms)
---     --   else go count xs
---   -- matches :: Coord -> Map Coord Char -> [Coord]
---   matches coord m =
---     let allWords = filter (\cs -> let word = snd <$> cs in word == "XMAS" || word == "SAMX") $ (\cs -> catMaybes $ (\c -> (c,) <$> Map.lookup c m) <$> cs) <$> potentialWords bounds coord
---      in allWords
-
 part2 :: Input -> Int
-part2 _ = 0
+part2 (Input bounds m) =
+  length $
+    filter (id) $
+      catMaybes $
+        (fmap (all isXmas . getWords) . potentialDiag bounds) <$> Map.keys m
+ where
+  isXmas :: String -> Bool
+  isXmas s = s == "MAS" || s == "SAM"
+  getWords :: [[Coord]] -> [String]
+  getWords ws = (\cs -> catMaybes $ flip Map.lookup m <$> cs) <$> ws
 
 makeInput :: String -> Input
 makeInput contents =
@@ -60,6 +52,9 @@ makeInput contents =
 main :: IO ()
 main = do
   input <- makeInput <$> readFile "input.txt"
+  let Input bounds m = input
+  print $ catMaybes $ potentialDiag bounds <$> Map.keys m
+
   print $ (part1 &&& part2) input
 
 potentialWords :: Bounds -> Coord -> [[Coord]]
@@ -75,18 +70,13 @@ potentialWords
         isInBounds (r, c) = r >= minRow && r <= maxRow && c >= minCol && c <= maxCol
      in filter (all isInBounds) coords
 
--- r r r r
-{-
--- First row
-row,col,   row,col + 1, row, col + 2, row, col + 3,
-
--- First column
-row,col, row + 1, col, row+2, col, row+3, col
-
--- diag forward down
-row,col, row+1,col+1, row+2,col+2, row+3,col+3
-
--- diag backward down
-row,col, row+1,col-1, row+2,col-2, row+3,col-3
-
--}
+potentialDiag :: Bounds -> Coord -> Maybe [[Coord]]
+potentialDiag
+  (Bounds (minRow, minCol) (maxRow, maxCol))
+  (row, col) =
+    let coords =
+          [ [(row - 1, col - 1), (row, col), (row + 1, col + 1)]
+          , [(row + 1, col - 1), (row, col), (row - 1, col + 1)]
+          ]
+        isInBounds (r, c) = r >= minRow && r <= maxRow && c >= minCol && c <= maxCol
+     in if all (all isInBounds) coords then Just coords else Nothing
