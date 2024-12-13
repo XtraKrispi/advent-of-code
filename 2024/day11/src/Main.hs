169bdbd8a682cc6df09380b9de300f0266848ff2
Michael Gold <mgold@spscommerce.com>
2024-12-13 10:31:51 -0500

    Day 11, Part 1

--- 2024/day11/src/Main.hs
+++ 2024/day11/src/Main.hs
@@ -5,16 +5,34 @@ import Data.Functor ((<&>))
 import Data.Maybe (fromMaybe, listToMaybe)
 import System.Environment (getArgs)
 
-type Input = ()
+type Stone = Int
+
+type Input = [Int]
+
+blink :: Input -> Input
+blink input = input >>= applyRule
+ where
+  applyRule :: Stone -> [Stone]
+  applyRule 0 = [1]
+  applyRule x
+    | even (length (show x)) =
+        let str = show x
+            (left, right) = splitAt (length str `div` 2) str
+         in [read left, read right]
+    | otherwise = [x * 2024]
+
+apply :: (a -> a) -> Int -> a -> a
+apply _ 0 val = val
+apply fn n val = apply fn (n - 1) (fn val)
 
 part1 :: Input -> Int
-part1 _ = 0
+part1 input = length $ apply blink 25 input
 
 part2 :: Input -> Int
 part2 _ = 0
 
 makeInput :: String -> Input
-makeInput _ = ()
+makeInput = (read <$>) . words
 
 main :: IO ()
 main =
