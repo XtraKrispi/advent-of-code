f7fd3b53f1f71f9632b7e5e676f1b4c48ecdc8c4
Michael Gold <mgold@spscommerce.com>
2025-12-02 10:20:12 -0500

    2025 - Day 2 Part 2 complete

--- 2025/day2/src/Main.hs
+++ 2025/day2/src/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Data.List (nub)
 import Data.Maybe (catMaybes)
 import Data.Text (pack, splitOn, unpack)
 
@@ -17,21 +18,48 @@ parseInput input = catMaybes $ fmap parseRange $ splitOn "," $ pack input
 part1 :: Input -> Integer
 part1 input = foldr (\range total -> total + sum (invalidNumbers range)) 0 input
  where
-  invalidNumbers (IdRange s e) = filter (not . isValid . show) [s .. e]
+  invalidNumbers (IdRange s e) = filter (not . isValidPart1 . show) [s .. e]
 
-isValid :: String -> Bool
-isValid x
+isValidPart1 :: String -> Bool
+isValidPart1 x
   | length x `mod` 2 /= 0 = True
   | otherwise =
       let leftHalf = take (length x `div` 2) x
           rightHalf = drop (length x `div` 2) x
        in leftHalf /= rightHalf
 
+part2 :: Input -> Integer
+part2 input = foldr (\range total -> total + sum (invalidNumbers range)) 0 input
+ where
+  invalidNumbers :: IdRange -> [Integer]
+  invalidNumbers (IdRange s e) = filter (not . isValidPart2 . show) [s .. e]
+
+isValidPart2 :: String -> Bool
+isValidPart2 x =
+  let factors = lengthFactors x
+   in not $ any (allEqual . chunk x) factors
+ where
+  allEqual :: (Eq a) => [[a]] -> Bool
+  allEqual xs = length (nub xs) == 1
+
+  chunk :: [a] -> Int -> [[a]]
+  chunk [] _ = []
+  chunk xs i = take i xs : chunk (drop i xs) i
+
+  lengthFactors :: String -> [Int]
+  lengthFactors i = filter (\i' -> (length i `mod` i' == 0)) [1 .. length i - 1]
+
 main :: IO ()
 main = do
-  putStrLn "Sample:"
-  sample <- readFile "sample.txt"
-  print $ part1 (parseInput sample)
-  putStrLn "Input:"
-  input <- readFile "input.txt"
-  print $ part1 (parseInput input)
+  sample <- parseInput <$> readFile "sample.txt"
+  putStrLn "Sample Part 1:"
+  print $ part1 sample
+
+  putStrLn "Sample Part 2:"
+  print $ part2 sample
+
+  input <- parseInput <$> readFile "input.txt"
+  putStrLn "Input Part 1:"
+  print $ part1 input
+  putStrLn "Input Part 2:"
+  print $ part2 input
