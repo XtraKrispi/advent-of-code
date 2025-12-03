97df7efc7a326dd5af6ad123154c8aaa8b65ea53
Michael Gold <mgold@spscommerce.com>
2025-12-03 14:54:48 -0500

    2025 - Day 3 Part 2, with logging

--- 2025/day3/src/Main.hs
+++ 2025/day3/src/Main.hs
@@ -1,24 +1,55 @@
 module Main (main) where
 
-import Data.List (sort, sortOn)
+import Control.Monad (join)
+import Control.Monad.Trans.Writer (Writer, runWriter, tell)
+import Data.IntMap qualified as IntMap
+import Data.List (nub, sortOn)
 
 type Joltage = Int
 
 type Bank = [Joltage]
 
-part1 :: [Bank] -> Int
-part1 banks = sum $ largestInBank <$> banks
+part1 :: [Bank] -> Integer
+part1 banks = sum $ determineLargestOfSize 2 <$> banks
 
-pairwiseVoltages :: Bank -> [Joltage]
-pairwiseVoltages [] = []
-pairwiseVoltages (x : rest) = something <> pairwiseVoltages rest
+part2 :: [Bank] -> Integer
+part2 banks = sum $ determineLargestOfSize 12 <$> banks
+
+determineLargestOfSize :: Int -> [Joltage] -> Integer
+determineLargestOfSize n = maximum . fmap toNum . fst . runWriter . go n
  where
-  something = (\y -> toNum [x, y]) <$> rest
+  go :: Int -> [Joltage] -> Writer [String] [Bank]
+  go n' joltages | n' <= 0 || n' > length joltages = do
+    pure [[]]
+  go n' joltages
+    | otherwise =
+        let
+          numbersAndIndices = reverse $ sortOn fst $ IntMap.toList $ foldr (\(idx, joltage) m -> IntMap.insertWith (++) joltage [idx] m) IntMap.empty $ zip [0 ..] joltages
+          applicableIndices = take 1 $ filter (\(_, indices) -> not $ null indices) $ (\(num, indices) -> (num, filter (\idx -> (length joltages - idx) >= n') indices)) <$> numbersAndIndices
+         in
+          do
+            -- newJoltages = case applicableIndices of
+            --   [(number, indices)] -> (\idx -> (number :) <$> go (n' - 1) (drop idx joltages)) =<< indices
+            --   _ -> []
+            tell $
+              [ "Joltages: " <> show joltages
+              , "N: " <> show n'
+              , "Numbers and Indices: " <> show numbersAndIndices
+              , "Applicable Indices: " <> show applicableIndices
+              ]
 
-largestInBank :: Bank -> Joltage
-largestInBank bank = maximum $ pairwiseVoltages bank
+            case applicableIndices of
+              [(number, indices)] ->
+                join
+                  <$> traverse
+                    ( \idx -> do
+                        results <- go (n' - 1) (drop (idx + 1) joltages)
+                        pure $ nub $ (number :) <$> results
+                    )
+                    indices
+              _ -> pure []
 
-toNum :: [Int] -> Int
+toNum :: [Int] -> Integer
 toNum = read . mconcat . (show <$>)
 
 parseInput :: String -> [Bank]
@@ -29,7 +60,9 @@ main = do
   putStrLn "Sample:"
   sample <- parseInput <$> readFile "sample.txt"
   putStrLn $ "Part 1: " <> show (part1 sample)
+  putStrLn $ "Part 2: " <> show (part2 sample)
 
   putStrLn "Input:"
   input <- parseInput <$> readFile "input.txt"
   putStrLn $ "Part 1: " <> show (part1 input)
+  putStrLn $ "Part 2: " <> show (part2 input)
