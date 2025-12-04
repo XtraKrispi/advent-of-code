fb521850199d651d5eff116301c1627ac648f96e
Michael Gold <mgold@spscommerce.com>
2025-12-04 15:30:39 -0500

    2025 - Day 4 Part 2 with Writer

--- 2025/day4/src/Main.hs
+++ 2025/day4/src/Main.hs
@@ -1,21 +1,23 @@
 module Main (main) where
 
-import Control.Monad (guard)
+import Control.Monad.Trans.Writer (Writer, runWriter, tell)
+import Data.Foldable (traverse_)
 import Data.Map (Map)
 import Data.Map qualified as Map
-import Data.Set (Set)
-import Data.Set qualified as Set
 
-newtype Row = Row {fromRow :: Int}
+newtype Row = Row Int
   deriving (Eq, Ord)
   deriving newtype (Show)
 
-newtype Col = Col {fromCol :: Int}
+newtype Col = Col Int
   deriving (Eq, Ord)
   deriving newtype (Show)
 type Coord = (Row, Col)
 
-type PaperGrid = Set Coord
+type PaperGrid = Map Coord Space
+
+data Space = Empty | Roll
+  deriving (Eq, Show)
 
 adjacentPositions :: Coord -> [Coord]
 adjacentPositions (Row row, Col col) =
@@ -31,33 +33,71 @@ adjacentPositions (Row row, Col col) =
 
 part1 :: PaperGrid -> Int
 part1 input =
-  Set.size $ Set.filter (isAccessible input) input
+  Map.size $ Map.filterWithKey (\c s -> if s == Empty then False else isAccessible input c) input
 
 isAccessible :: PaperGrid -> Coord -> Bool
 isAccessible grid coord =
   let adjacent = adjacentPositions coord
-   in (< 4) $ length $ filter (\c -> Set.member c grid) adjacent
+   in (< 4) $ length $ filter (\c -> maybe False (== Roll) $ Map.lookup c grid) adjacent
 
-part2 :: PaperGrid -> Int
-part2 input = 0
+rollsOnly :: PaperGrid -> PaperGrid
+rollsOnly = Map.filter (== Roll)
+
+part2 :: PaperGrid -> (Int, [String])
+part2 input = runWriter $ go 0 (Map.size $ rollsOnly input) input
+ where
+  go :: Int -> Int -> PaperGrid -> Writer [String] Int
+  go removed prev grid = do
+    tell ["In go with params: Removed: " <> show removed <> ", Prev: " <> show prev, prettyGrid grid]
+    let accessible = Map.filterWithKey (\c s -> if s == Empty then False else isAccessible grid c) grid
+        numAccessible = Map.size accessible
+    tell ["accessible: " <> show accessible, "number of accessible: " <> show numAccessible]
+    if Map.size accessible == 0
+      then
+        pure removed
+      else do
+        let updatedGrid = Map.foldrWithKey (\coord _ -> Map.insert coord Empty) grid accessible
+        go (removed + numAccessible) numAccessible updatedGrid
 
 parseInput :: String -> PaperGrid
 parseInput input =
-  Set.fromList $
+  Map.fromList $
     do
       (rowIdx, row) <- zip [0 ..] (lines input)
       (colIdx, col) <- zip [0 ..] row
-      guard $ col == '@'
-      pure (Row rowIdx, Col colIdx)
+      pure ((Row rowIdx, Col colIdx), if col == '@' then Roll else Empty)
+
+prettyGrid :: PaperGrid -> String
+prettyGrid grid =
+  let minCoord = Map.lookupMin grid
+      maxCoord = Map.lookupMax grid
+   in case (minCoord, maxCoord) of
+        (Just ((Row minRow, Col minCol), _), Just ((Row maxRow, Col maxCol), _)) ->
+          unlines $
+            ( \row ->
+                ( \col ->
+                    case Map.lookup (Row row, Col col) grid of
+                      Nothing -> 'x'
+                      Just Empty -> '.'
+                      Just Roll -> '@'
+                )
+                  <$> [minCol .. maxCol]
+            )
+              <$> [minRow .. maxRow]
+        _ -> ""
 
 main :: IO ()
 main = do
   putStrLn "Sample: "
   sample <- parseInput <$> readFile "sample.txt"
   putStrLn $ "Part 1: " <> show (part1 sample)
-  putStrLn $ "Part 2: " <> show (part2 sample)
+  let part2Sample = part2 sample
+  putStrLn $ "Part 2: " <> show (fst part2Sample)
+  putStrLn "Log: "
+  traverse_ putStrLn (snd part2Sample)
 
   putStrLn "Input: "
   input <- parseInput <$> readFile "input.txt"
   putStrLn $ "Part 1: " <> show (part1 input)
-  putStrLn $ "Part 2: " <> show (part2 input)
+
+  putStrLn $ "Part 2: " <> show (fst $ part2 input)
